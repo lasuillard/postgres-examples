16c3f8dfd6e04a0a04d38bc543eb2847a5428df6
Yuchan Lee <lasuillard@gmail.com>
2024-06-05 22:15:24 +0900

    feat: Add aws_s3 extension usage example

---
+++ aws-s3-extension/export-s3.sql
@@ -0,0 +1,11 @@
+SELECT * FROM aws_s3.query_export_to_s3(
+    query := 'select * from store',
+    bucket := 'bucket',
+    file_path := 'store.csv',
+    region := NULL,
+    access_key := 'user',
+    secret_key := 'password', 
+    session_token := NULL,
+    options := 'FORMAT CSV, HEADER',
+    endpoint_url := 'http://minio:9000'
+);
