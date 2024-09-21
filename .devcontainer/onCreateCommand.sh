f0fd4d6dbc704d81b8a987700d501e24b2cad7bf
Yuchan Lee <lasuillard@gmail.com>
2024-09-21 22:24:45 +0900

    chore(devcontainer): Update `onCreateCommand.sh`
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -5,15 +5,3 @@ curl https://pyenv.run | bash
 
 sudo apt-get update && sudo apt-get install -y \
     bash-completion
-
-echo '
-if [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-fi
-
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-eval "$(pyenv virtualenv-init -)"
-' >>~/.bashrc
